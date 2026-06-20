63ebaf2cdde2ed8d50351e529fbac0ef16248936
Bruno Costa <bcosta17.bc@gmail.com>
2026-06-20 15:58:04 -0300

    criação da classe para salvar os dados de Usuário no banco

---
+++ src/infrastructure/persistence/entities/user_entity.ts
@@ -0,0 +1,11 @@
+import { Column, Entity, PrimaryColumn, } from "typeorm";
+
+@Entity("users")
+export class UserEntity {
+    @PrimaryColumn("uuid")
+    id!: string;
+
+    @Column()
+    name!: string;
+    
+}
---
+++ src/infrastructure/repositories/typeorm_user_repository.test.ts
@@ -0,0 +1,52 @@
+import { User } from "../../domain/entities/user";
+import { DataSource, Repository } from "typeorm";
+import { UserEntity } from "../persistence/entities/user_entity";
+import { TypeOrmUSerRepository } from "./typerorme_user_repository";
+
+describe("TypeORM User Repository", () => {
+    let dataSource: DataSource;
+    let userRepository: TypeOrmUSerRepository;
+    let repository: Repository<UserEntity>;
+
+    beforeAll(async () => {
+        dataSource = new DataSource({
+            type: "sqlite",
+            database: ":memory:",
+            dropSchema: true,
+            entities: [UserEntity],
+            synchronize: true,
+            logging: false,
+        });
+        await dataSource.initialize();
+        repository = dataSource.getRepository(UserEntity);
+        userRepository = new TypeOrmUSerRepository(repository);
+    })
+
+    afterAll(async () => {
+        await dataSource.destroy();
+    });
+
+    it("deve salvar um usuário com sucesso", async () => {
+        const user = new User( "1", "John Doe");
+        await userRepository.save(user);
+
+        const savedUser = await repository.findOne({where: { id: "1" } });
+        expect(savedUser).not.toBeNull();
+        expect(savedUser?.id).toBe("1");
+    })
+    
+    it("deve retornar um usuário quando um ID válido for fornecido", async () => {
+        const user = new User("1", "John Doe");
+        await userRepository.save(user);
+
+        const foundUser = await userRepository.findById("1");
+        expect(foundUser).not.toBeNull();
+        expect(foundUser?.getId()).toBe("1");
+    });
+
+    it("deve retornar null quando um ID inválido for fornecido", async () => {
+        const foundUser = await userRepository.findById("999");
+        expect(foundUser).toBeNull();
+    });
+}); 
+      
