dca6bbe980b2f3d22d16cd220d600e33f7b2f849
Bruno Costa <bcosta17.bc@gmail.com>
2026-06-21 16:04:41 -0300

    implemetação do property repository

---
+++ src/infrastructure/persistence/entities/property_entity.ts
@@ -0,0 +1,20 @@
+import {Entity, PrimaryColumn, Column} from "typeorm";
+
+@Entity("properties")
+export class PropertyEntity {
+    
+    @PrimaryColumn("uuid")
+    id!: string;
+
+    @Column()
+    name!: string;
+
+    @Column()
+    description!: string;
+
+    @Column({name: "max_guests"})
+    maxGuests!: number;
+
+    @Column({name: "base_price_per_night", type: "decimal"})
+    basePricePerNight!: number;
+}
---
+++ src/infrastructure/repositories/typeorm_property_repository.test.ts
@@ -0,0 +1,54 @@
+import { DataSource, Repository } from "typeorm";
+import { PropertyEntity } from "../persistence/entities/property_entity";
+import { UserEntity } from "../persistence/entities/user_entity";
+import { Property } from "../../domain/entities/property";
+import { TypeOrmPropertyRepository } from "./typeorm_property_repository";
+
+describe("TypeORM Property Repository", () => {
+    let dataSource: DataSource;
+    let propertyRepository: TypeOrmPropertyRepository;
+    let repository: Repository<PropertyEntity>;
+
+    beforeAll(async () => {
+        dataSource = new DataSource({
+            type: "sqlite",
+            database: ":memory:",
+            dropSchema: true,
+            entities: [UserEntity, PropertyEntity],
+            synchronize: true,
+            logging: false,
+        });
+        await dataSource.initialize();
+        repository = dataSource.getRepository(PropertyEntity);
+        propertyRepository = new TypeOrmPropertyRepository(repository);
+    })
+
+    afterAll(async () => {
+        await dataSource.destroy();
+    });
+
+    it("deve salvar uma propriedade com sucesso", async () => {
+        const property = new Property("1", "Casa", "Uma casa bonita", 1000, 4);
+        
+        await propertyRepository.save(property);
+
+        const savedProperty = await repository.findOne({ where: { id: "1" } });
+        expect(savedProperty).not.toBeNull();
+        expect(savedProperty?.id).toBe("1");
+    });
+
+    it("deve retornar uma propriedade quando um ID válido for fornecido", async () => {
+        const property = new Property("1", "Casa", "Uma casa bonita", 1000, 4);
+        await propertyRepository.save(property);    
+
+        const foundProperty = await propertyRepository.findById("1");
+        expect(foundProperty).not.toBeNull();
+        expect(foundProperty?.getId()).toBe("1");
+    });
+
+    it("deve retornar null quando um ID inválido for fornecido", async () => {
+        const foundProperty = await propertyRepository.findById("999");
+        expect(foundProperty).toBeNull();
+    });
+
+});
